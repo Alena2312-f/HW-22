dfde3a564d03e16dd51c8d1e17bacfcbc8e62a63
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-09-13 19:50:23 +0300

    HW DONE

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
+    """Kонтроллер для отображения домашней страницы"""
+
     return render(request, "home.html")
 
 
 def contacts(request):
+    """Контроллер для отображения страницы с контактной информацией"""
+
     return render(request, "contacts.html")
--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,5 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Медиатека (Media)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
