1993146274f754f0e98fe40e9b31429d6afa7e11
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-09-19 22:46:07 +0300

    HW

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 class HomeView(TemplateView):
@@ -12,13 +14,36 @@ class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
 
 
-
 class ProductListView(ListView):
-    """Контроллер для отображения страницы с продукцией"""
     model = Product
+    template_name = 'catalog/product.html'
+    context_object_name = 'page_obj'
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_products'] = Product.objects.all().order_by('-created_at')[:5]
+        return context
 
 
 class ProductDetailView(DetailView):
-    """Контроллер для отображения страницы с отдельной продукцией"""
     model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
 
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product')
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product')
