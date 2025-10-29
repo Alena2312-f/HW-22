e573d43620e982c42ccf58db7f673b128c6c27a0
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-10-29 22:18:48 +0300

    HW 27.1

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,9 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'users:login'
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "Alena.Sarycheva99@yandex.ru"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
