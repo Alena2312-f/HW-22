6d8d570be9ceb35f5e61cd86dc1d696171049cca
Alena Chaschevaia <alena.sarycheva.99@bk.ru>
2025-10-30 21:58:25 +0300

    HW 27.1

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name='login.html'), name='login'),
-    path("logout/", LogoutView.as_view(next_page='home'), name='logout'),
+    path("logout/", LogoutView.as_view(), name='logout'),
     path("register/", UserCreateView.as_view(), name='register')
 
 ]
